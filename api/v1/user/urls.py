bddc92b645efb8fe4ad1607f4544f07cba2bc2cd
esheloncr <igoninalexeynn@gmail.com>
2023-01-02 15:20:36 +0300

    Add authentication API endpoint

--- api/v1/user/urls.py
+++ api/v1/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserDetailView, UserListView, UserCreateView
+from .views import UserDetailView, UserListView, UserCreateView, UserAuthenticateView
 
 
 urlpatterns = [
     path("", UserListView.as_view(), name="users_list"),
     path("<int:pk>/", UserDetailView.as_view(), name="user_detail"),
-    path("create/", UserCreateView.as_view(), name="create_user")
+    path("create/", UserCreateView.as_view(), name="create_user"),
+    path("login/", UserAuthenticateView.as_view(), name="login_user")
 ]
--- api/v1/user/views.py
+++ api/v1/user/views.py
@@ -1,8 +1,9 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, GenericAPIView
+from rest_framework.response import Response
 
 from user.models import User
 
-from .serializers import UserListSerializer, UserDetailSerializer, UserCreateSerializer
+from .serializers import UserListSerializer, UserDetailSerializer, UserCreateSerializer, UserAuthenticationSerializer
 
 
 class UserDetailView(RetrieveAPIView):
@@ -18,3 +19,16 @@ class UserListView(ListAPIView):
 class UserCreateView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
+
+
+class UserAuthenticateView(GenericAPIView):
+    serializer_class = UserAuthenticationSerializer
+    queryset = User.objects.exclude(is_superuser=True)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = {
+            "token": serializer.token
+        }
+        return Response(data)
